aa0451032f0b525e88b0fd9fa039c9cab172bc0b
rnoviansyah <rizkynoviansyah5@gmail.com>
2026-08-10 13:05:34 +0700

    Add files via upload

--- api/config.js
+++ api/config.js
@@ -1,1 +1,10 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
 
+  res.status(200).json({
+    supabaseUrl: process.env.SUPABASE_URL || '',
+    supabaseKey: process.env.SUPABASE_KEY || ''
+  });
+};
